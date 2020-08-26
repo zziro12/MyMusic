794434878156c2a9502d2170555dcc625fb932b2
zziro12 <opers7@naver.com>
2020-08-26 16:56:06 +0900

    start MyMusic App

--- MyMusic/Home/HomeViewController.swift
+++ MyMusic/Home/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  MyMusic
 //
 //  Created by Jinho Choi on 2020/08/26.
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
-
