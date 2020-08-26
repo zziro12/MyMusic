78cba7c2260d53b24ffe439412575a6accd9bac7
zziro12 <opers7@naver.com>
2020-08-26 16:28:53 +0900

    Initial Commit

---
+++ MyMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by Jinho Choi on 2020/08/26.
+//  Copyright © 2020 Jinho Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
